2660d365cbc636d4d2da9dab99726caff8326719
Snake Man <snakeman@phnet.fi>
2015-02-03 04:45:41 +0200

    initial commit

---
+++ widget.cpp
@@ -0,0 +1,57 @@
+#include "widget.h"
+#include "ui_widget.h"
+
+#include <QtWidgets>
+
+Widget::Widget(QWidget *parent) :
+	QWidget(parent),
+	ui(new Ui::Widget)
+{
+	ui->setupUi(this);
+	OpenFile();
+	DisplayShit();
+}
+
+Widget::~Widget()
+{
+	delete ui;
+}
+
+
+void Widget::OpenFile()
+{
+	QFile file( ("d://coding//test_files//elite_csv_1.csv") );
+
+	if (!file.open(QIODevice::ReadOnly))
+	{
+		QMessageBox::information(this, tr("Unable to open file"),
+		file.errorString());
+		return;
+	}
+
+	QString line;
+	QTextStream in(&file);
+
+	while (!in.atEnd())
+	{
+		line = in.readLine();
+		commodities.push_back(line);
+	}
+	file.close();
+}
+
+
+void Widget::DisplayShit()
+{
+	QStringList list;
+	int i = 0;
+	while (i < commodities.size())
+	{
+		list = commodities[i].split(";");
+		i++;
+
+		ui->textEdit->append("System: " + list[0] + "\nStation: " + list[1] + "\nCommodity: " + list[2] + "\nSell: " + list[3] + "\nBuy: " + list[4]
+				+ "\nDemand: " + list[5] + "\nDemand chars???: " + list[6] + "\nSupply: " + list[7] + "\nSupply chars???: " + list[8]
+				+ "\nDateTime: " + list[9]);
+	}
+}
